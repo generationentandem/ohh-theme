fba3234d7d029c017bc1e505e532e76c07d7f866
Vento <github@brenneisen.me>
2023-04-18 19:37:17 +0200

    Create und-card-post.php

---
+++ theme/template-parts/block/und-card-post.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+$featured = get_query_var('additional_classes',false);
+$classes = 'card post-card ' . (($featured)?$featured:'');
+?>
+<article id='post-<?php echo $post['id'] ?>' <?php post_class( $classes ) ?> itemscope itemtype="https://schema.org/Article">
+	<a href="<?php the_permalink(); ?>" class="card-image">
+        <img style="height: 12em" src="<?php echo $post['_embedded']['wp:featuredmedia'][0]['source_url'] ?>" />
+	</a>
+	<section class="card-section">
+		<div class="card-meta">
+			<time itemprop="datePublished" title="<?php echo date_i18n( "l, d. F Y | H:i:s", get_the_date( "U" ) ) ?>"
+					datetime="<?php echo get_the_date( DATE_W3C ) ?>"><?php echo relative_date( DateTime::createFromFormat( "U", get_the_date( 'U' ) ) ) ?></time>
+		</div>
+		<a href="<?php the_permalink(); ?>" class="card-link">
+			<?php
+			$title = $post['title']['rendered'];
+            $has_thumbnail = $post['_embedded']['wp:featuredmedia'][0]['source_url'] == false;
+			echo "<h2 itemprop='name' class='card-title h4 " . ( ! $has_thumbnail && strlen( $title ) < 30 && get_longest_word( $title ) < 14 ? "" : "small" ) . "'>${title}</h2>";
+			?>
+		</a>
+		<?php if ( ! $has_thumbnail && get_post()->post_type != 'page' && strlen( $title ) < 50 ) { ?>
+			<div class="card-excerpt" itemprop="description">
+				<?php
+				    echo $post['excerpt']['rendered'];
+                ?>
+			</div>
+			<?php
+		}
+		?>
+	</section>
+</article>
