bd3cd8fdd40b9959f2883ed5256a5a2d7dfd93cd
Vento <github@brenneisen.me>
2023-04-21 16:51:58 +0200

    Update aktueller-event.php

--- theme/page-templates/aktueller-event.php
+++ theme/page-templates/aktueller-event.php
@@ -3,8 +3,18 @@
 Template Name: Aktueller Event
 */
 
-setlocale(LC_TIME, 'de_DE', 'deu_deu');
+setlocale(LC_TIME, 'de_DE.UTF-8', 'deu_deu');
 define( 'TWOHOURS', 7200 );
+
+$args = [
+    'headers' => [
+        'method' => 'GET',
+    ]
+];
 
 get_header();
 
+
+
+get_footer();
+?>
