dc653d8057aefe277fe95fa7dd27f8eb23830580
Vento <github@brenneisen.me>
2023-04-24 15:04:52 +0200

    Update content-page.php

--- theme/template-parts/content-page.php
+++ theme/template-parts/content-page.php
@@ -10,22 +10,23 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header>
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-	</div>
-	<footer>
-		<?php
-			wp_link_pages(
-				array(
-					'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-					'after'  => '</p></nav>',
-				)
-			);
-		?>
-		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
-	</footer>
+    <header class="entry-header">
+        <div class="post-wrapper">
+            <?php
+            if ( in_category( 9218 ) ) {
+                echo '<h3 class="entry-title h6 text-center">Veranstaltung</h3>';
+            }
+            ?>
+            <h1 class="entry-title alignwide">
+                <?php the_title(); ?>
+            </h1>
+            <?php the_lead() ?>
+        </div>
+    </header>
+    <section class="entry-content">
+        <div class="post-wrapper">
+            <?php the_content(); ?>
+            <?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+        </div>
+    </section>
 </article>
