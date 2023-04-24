5de7a28d9856958c80f773e3443f0167e0ab010f
Vento <github@brenneisen.me>
2023-04-24 10:13:31 +0200

    Update menu-walkers.php

--- theme/library/menu-walkers.php
+++ theme/library/menu-walkers.php
@@ -6,25 +6,23 @@
  * @since FoundationPress 1.0.0
  */
 
- // Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
+// Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
 
- if ( ! class_exists( 'Foundationpress_Top_Bar_Walker' ) ) :
- class Foundationpress_Top_Bar_Walker extends Walker_Nav_Menu {
+if ( ! class_exists( 'Foundationpress_Top_Bar_Walker' ) ) :
+    class Foundationpress_Top_Bar_Walker extends Walker_Nav_Menu {
+ 	    function start_lvl( &$output, $depth = 0, $args = array() ) {
+            $indent = str_repeat("\t", $depth);
+            $output .= "\n$indent<ul class=\"dropdown menu vertical\" data-toggle>\n";
+ 	    }
+    }
 
- 	function start_lvl( &$output, $depth = 0, $args = array() ) {
- 			$indent = str_repeat("\t", $depth);
-      $output .= "\n$indent<ul class=\"dropdown menu vertical\" data-toggle>\n";
- 	}
- }
 
-
- if ( ! class_exists( 'Foundationpress_Mobile_Walker' ) ) :
- class Foundationpress_Mobile_Walker extends Walker_Nav_Menu {
- 	function start_lvl( &$output, $depth = 0, $args = array() ) {
- 			$indent = str_repeat("\t", $depth);
- 			$output .= "\n$indent<ul class=\"vertical nested menu\">\n";
- 	}
- }
- endif;
-
+    if ( ! class_exists( 'Foundationpress_Mobile_Walker' ) ) :
+        class Foundationpress_Mobile_Walker extends Walker_Nav_Menu {
+            function start_lvl( &$output, $depth = 0, $args = array() ) {
+                $indent = str_repeat("\t", $depth);
+                $output .= "\n$indent<ul class=\"vertical nested menu\">\n";
+            }
+        }
+    endif;
 endif;
