5ac4793d505409e814632acaba04dd7a62981cda
Vento <github@brenneisen.me>
2023-07-04 16:04:19 +0200

    Update einsatzplan.php

--- theme/page-templates/einsatzplan.php
+++ theme/page-templates/einsatzplan.php
@@ -34,7 +34,82 @@ $args = array(
     ),
 );
 
+/**
+ * List Rows in A View in A Table
+ * List all the rows and their content in a specific view in a table.
+ * https://api.seatable.io/#c7caa77d-6214-4ca1-bb91-5c1d3d19c52d
+ *
+ * PARAMS:
+ * table_name:  Standort Thun | Standort Thun Rathaus | Standort Bern | Standort Interlaken
+ * view_name:   WordPress
+ */
+$response            = wp_remote_request( 'https://seatable.generationentandem.ch/dtable-server/api/v1/dtables/45bdb836-036f-44f8-a615-d33ebdd52a43/rows/?table_name=Einsatzplan&view_name=Wordpress&convert_link_id=true&order_by=Datum&direction=asc&limit=365', $args );
+$table_location_rows = json_decode( $response['body'], true );
+
+/**
+ * List Columns in A View in A Table
+ * List all the visible columns in a certain view in a table.
+ * https://api.seatable.io/#bc069ee4-95a3-4204-9ce7-ae7c65469377
+ *
+ * PARAMS:
+ * table_name:  Standort Thun | Standort Thun Rathaus | Standort Bern | Standort Interlaken
+ * view_name:   WordPress
+ */
+$response               = wp_remote_request( 'https://seatable.generationentandem.ch/dtable-server/api/v1/dtables/45bdb836-036f-44f8-a615-d33ebdd52a43/columns/?table_name=Einsatzplan&view_name=WordPress', $args );
+$table_location_columns = json_decode( $response['body'], true );
+
+/**
+ * List Rows in A View in A Table
+ * List all the rows and their content in a specific view in a table.
+ * https://api.seatable.io/#c7caa77d-6214-4ca1-bb91-5c1d3d19c52d
+ *
+ * PARAMS:
+ * table_name:  HelferInnen
+ * view_name:   WordPress
+ */
+$response              = wp_remote_request( 'https://seatable.generationentandem.ch/dtable-server/api/v1/dtables/45bdb836-036f-44f8-a615-d33ebdd52a43/rows/?table_name=HelferInnen&view_name=Wordpress', $args );
+$table_volunteers_rows = json_decode( $response['body'], true );
+
+/**
+ * For earlier versions of PHP, polyfill of the str_contains function.
+ */
+if ( ! function_exists( 'str_contains' ) ) {
+    function str_contains( string $haystack, string $needle ) {
+        return '' !== $needle && mb_strpos( $haystack, $needle ) !== false;
+    }
+}
+
+/**
+ * Sets locale information.
+ */
+setlocale( LC_ALL, 'de_DE.UTF-8' );
+
+/**
+ * In which week is the respective day.
+ */
+$weekly_schedules = array_unique(
+    array_map(
+        function( $val ) {
+            return date( 'W', strtotime( $val['Datum'] ) );
+        },
+        $table_location_rows['rows']
+    )
+);
+
+/**
+ * Anonymize the last names of the helpers.
+ */
+$helferinn = function( $name ) {
+    return implode( ' ', explode( ' ', trim( $name ), -1 ) ) . ' ' . substr( end( explode( ' ', trim( $name ) ) ), 0, 1 ) . '.';
+};
+
 $selected_location = ( intval( $input_location ) > '0' && intval( $input_location ) < '5' ? intval( $input_location ) : '1' );
+
+$input_week      = filter_input( INPUT_POST, 'week', FILTER_VALIDATE_INT );
+
+$current_date  = new DateTime();
+$current_week  = date( 'W', strtotime( $current_date->format( 'Y-m-d' ) ) );
+$selected_week = ( intval( $input_week ) > '0' && intval( $input_week ) < '53' ? intval( $input_week ) : intval( $current_week ) );
 
 ?>
 
@@ -56,7 +131,12 @@ $selected_location = ( intval( $input_location ) > '0' && intval( $input_locatio
             <form action="#plan" method="post">
                 <label for='week'>Wähle den gewünschten Wochenplan aus:</label>
                 <select name="week" id="week" onchange="this.form.submit()">
-                    <option></option>
+                    <?php
+                        foreach($weekly_schedules as $row) {
+                            $html = '<option value="%d" %s> Wochenplan %d (%s &#x2013; %s)</option>';
+                            $is_selected = ($row == $selected_week ? 'selected="selected"' : '');
+                        }
+                    ?>
                 </select>
                 <input type="hidden" id="location" name="location" value="<?php echo $selected_location; ?>">
             </form>
